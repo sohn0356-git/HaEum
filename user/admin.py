1232c0b584e719e2e0d3af4c8bfb4add01ab313e
sohn0356-git <sohn0356@gmail.com>
2021-02-17 22:54:29 +0900

    Modify index.html user_list

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from user.models import User
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+
+admin.site.register(User, UserAdmin)
--- user/views.py
+++ user/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from user.models import User
 # Create your views here.
+
 def index(request):
-    return render(request, 'index.html', { 'email': request.session.get('user') })
+    user_list = User.objects.all()
+    res_data = {'user_list' : user_list, 'email': request.session.get('user')}
+    return render(request, 'index.html', res_data)
